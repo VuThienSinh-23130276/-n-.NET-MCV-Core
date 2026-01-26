26002fd83bc7657d1c9b2567a4fc927ea1136caf
ThuanTa321 <23130321@st.hcmuaf.edu.vn>
2026-01-26 23:12:40 +0700

    cc

---
+++ Do_An_Net_CuoiKy/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Do_An_Net_CuoiKy.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Sau này có thể thêm FullName, Avatar... nếu cần
+    }
+}
